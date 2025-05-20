aeed4451511875e5acdd72f18e98f8c95c24900c
Jerry Morey <themorey@gmail.com>
2025-05-20 16:11:40 -0400

    Updated Slurm template and client script to support 3.0.11

--- specs/client/cluster-init/scripts/001-weka-client-install.sh
+++ specs/client/cluster-init/scripts/001-weka-client-install.sh
@@ -44,4 +44,14 @@ curl http://${weka_address}:14000/dist/v1/install | sh
 
 # You can mount a stateless or stateful client on the filesystem using UDP. The following is an example of mounting a stateless client: 
 # mount -t wekafs -o net=udp <Load balancer DNS name or IP address>/<filesystem name> /mnt/weka
-mount -t wekafs -o net=udp,num_cores=1 ${weka_address}/${fs} ${mount_point}
+
+# support for multiple NICs for DPDK mode was added to CC ver 8.7.0 and Slurm project version 3.0.10
+version=$(jetpack config slurm.autoscale_version)
+if [[ $(echo -e "$version\n3.0.10" | sort -V | head -n1) == "3.0.10" ]] || [[ "$version" == "3.0.10" ]]; then
+    echo "Version $version supports DPDK"
+    mount -t wekafs -o num_cores=1 ${weka_address}/${fs} ${mount_point}
+else
+    echo "Version $version does not support DPDK...using UDP mode"
+    mount -t wekafs -o net=udp,num_cores=1 ${weka_address}/${fs} ${mount_point}
+fi
+
